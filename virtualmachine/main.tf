ea2cc3f309a7a3212736e5233e054c4f3d50cd7d
IylaveniNiharika <niharikaiylaveni32@gmail.com>
2025-12-23 19:03:04 +0530

    Update main.tf

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = var.public_key
+    public_key = file(var.public_key)
   }
 
   os_disk {
@@ -24,3 +24,4 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 
 }
+
