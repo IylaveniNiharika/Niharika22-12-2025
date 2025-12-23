2be1c89793483f6d5eca0b998bed0ad25dd442e5
IylaveniNiharika <niharikaiylaveni32@gmail.com>
2025-12-23 17:06:10 +0530

    Add files via upload

---
+++ nic/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_network_interface" "example" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
---
+++ nic/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type = string
+}
+variable "nic_name" {
+  type = string
+}
+variable "resource_group" {
+  type = string
+}
