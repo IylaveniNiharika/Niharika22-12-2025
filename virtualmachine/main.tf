0d328ea405d7b4ab1ab10ea44e512c97c8dc9fbd
IylaveniNiharika <niharikaiylaveni32@gmail.com>
2025-12-23 18:38:09 +0530

    Add files via upload

---
+++ virtualmachine/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = "adminuser"
+  network_interface_ids = [var.nic_id,]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ virtualmachine/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  type = string
+}
+variable "resource_group" {
+  type = string
+}
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+variable "nic_id" {
+  type = string
+}
