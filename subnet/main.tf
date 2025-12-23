5dbaee07ea784e219e4fd103ea79f0425501b8a1
IylaveniNiharika <niharikaiylaveni32@gmail.com>
2025-12-23 16:09:16 +0530

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_subnet" "example" {
   name                 = var.subnet_name
-  resource_group_name  = var.rg_name
+  resource_group_name  = var.resource_group
   virtual_network_name = var.vnet_name
   address_prefixes     = var.subnet_prefix
+
 }
