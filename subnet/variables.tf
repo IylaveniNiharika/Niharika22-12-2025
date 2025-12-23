87427a7a21063214324f63c8fb2afbae98045a44
IylaveniNiharika <niharikaiylaveni32@gmail.com>
2025-12-23 15:57:05 +0530

    Add files via upload

---
+++ subnet/variables.tf
@@ -0,0 +1,15 @@
+variable "subnet_prefix" {
+  type = list(string)
+}
+variable "subnet_name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "rg_name" {
+  type = string
+}
+variable "vnet_name" {
+  type = string
+}
